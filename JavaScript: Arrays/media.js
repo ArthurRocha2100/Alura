7db155b3a480aee5576793f5db31a27fbe3fb3c7
ArthurRocha2101 <arthurrocha2101@gmail.com>
2023-01-05 21:17:52 -0300

    aula 01. O que são Arrays

---
+++ JavaScript: Arrays/media.js
@@ -0,0 +1,7 @@
+const notas = [10,5,7,3]; // Lista de notas
+const media = (notas[0] + notas[1] + notas [2] + notas [3]) / notas.length; // notas.length é o "Tamanho do Array"
+console.log(media);
+
+
+const listaDeCompras = ["banana","tomate","uva","limão","arroz"];
+console.log(listaDeCompras);
