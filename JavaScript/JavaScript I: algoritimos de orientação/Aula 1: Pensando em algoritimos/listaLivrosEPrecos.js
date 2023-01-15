2b129f4e12ea83c14e7e511dc91ff2749d5eaf8a
ArthurRocha2101 <arthurrocha2101@gmail.com>
2023-01-15 13:03:06 -0300

    Aula 1 JavaScript I: Algoritimos de orientação

---
+++ JavaScript/JavaScript I: algoritimos de orientação/Aula 1: Pensando em algoritimos/listaLivrosEPrecos.js
@@ -0,0 +1,25 @@
+class Livro {
+    constructor(titulo, preco){
+        this.titulo = titulo,
+        this.preco = preco
+    };
+    
+    getTitulo(){
+        return this.titulo
+    };
+    getPreco(){
+        return this.getPreco
+    };
+};
+
+const listaLivros = [
+    livroJavaScript = new Livro ("javaScript", 25),
+    livroPHP = new Livro("PHP", 15),
+    livroJava = new Livro("java", 30),
+    livroElixir = new Livro("Elixir", 50),
+    livroGo = new Livro("Go", 45),
+    livroPython = new Livro("Python", 20)
+
+];
+
+module.exports = listaLivros;
